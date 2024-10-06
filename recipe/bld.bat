082c01fae142b7c616051f6db1de28bde5591de7
Mark Piper <mark.piper@colorado.edu>
2024-10-06 12:14:57 -0600

    Tweak to build on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 mkdir build && cd build
 
-cmake -LAH -G "NMake Makefiles" ^
+cmake -G "NMake Makefiles" ^
   -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE=Release ^
