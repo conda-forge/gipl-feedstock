a064226b98c28acf894dc43443c22bd909cc1a26
Mark Piper <mark.piper@colorado.edu>
2024-12-13 10:26:31 -0700

    Use Ninja build backend

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,14 @@
-mkdir build && cd build
+set "BUILD_DIR=_build"
 
-cmake -LAH -G "NMake Makefiles" ^
+cmake -G Ninja ^
   -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE=Release ^
-  %CMAKE_ARGS% ^
-  %SRC_DIR%
-if errorlevel 1 exit 1
+  %CMAKE_ARGS%
+if %ERRORLEVEL% neq 0 exit 1
 
-cmake --build . --target install --config Release
-if errorlevel 1 exit 1
+cmake --build %BUILD_DIR%
+if %ERRORLEVEL% neq 0 exit 1
+
+cmake --install %BUILD_DIR%
+if %ERRORLEVEL% neq 0 exit 1
