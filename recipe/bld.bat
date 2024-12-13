cbccaac74a0c248d309682cf86b1e4def495ff3a
Mark Piper <mark.piper@colorado.edu>
2024-12-13 16:29:17 -0700

    Set -Mbackslash for flang 5

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,8 @@
 set "BUILD_DIR=_build"
 
+:: A fix for flang 5. It's not needed for flang 19.
+set "FFLAGS=-Mbackslash"
+
 cmake -LA -B %BUILD_DIR% -G Ninja ^
   -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
